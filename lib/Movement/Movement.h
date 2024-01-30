1f6cc7d34fde5cd7c8743777b188206c07313435
Jon Chen <bsd@voltaire.sh>
2024-01-30 16:51:41 +0800

    add more documentation

--- lib/Movement/Movement.h
+++ lib/Movement/Movement.h
@@ -10,6 +10,7 @@ void fullstop();
 
 // pwm stuff
 // for more on what the values mean: https://forums.raspberrypi.com/viewtopic.php?t=309632
+// https://forums.raspberrypi.com/viewtopic.php?f=145&t=307912#p1844432
 #define MOTOR_ENABLE_FREQ 1500
 #define MOTOR_CLK_DIV 1
 #define MOTOR_WRAP_VALUE (125000000 / (MOTOR_CLK_DIV * MOTOR_ENABLE_FREQ)) - 1
